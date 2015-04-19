d416943f188b525e546deed05dd5f25c31c7af7c
galbened <galbened@post.bgu.ac.il>
2015-04-19 17:02:19 +0300

    try

---
+++ Testing/Testing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            for (; ; )
+            {
+                Console.WriteLine("Welcome to GIT.");
+                input = Console.ReadLine();
+                if (input.Equals("q"))
+                    break;
+            }
+        }
+    }
+}
